3721c354f8efa444dc7853f16b41002f0c7c12d6
JimJ0112 <68044290+JimJ0112@users.noreply.github.com>
2023-02-11 12:50:39 +0800

    second commit

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -13,7 +13,14 @@ echo "Connection Fail".mysqli_connect_error();
 */
 
 // jim
+/*
 $con=mysqli_connect("localhost:3307", "root", "", "vpmsdb_v2");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+*/
+
+$con=mysqli_connect("localhost", "root", "", "vpmsdb_v2");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
